04b01870529a2709d7bf102056c1be71c5a0b01b
Lusine Bars <bars.lucy@gmail.com>
2013-06-12 10:30:23 -0700

    homework 19 and 20

--- run.sh
+++ run.sh
@@ -13,39 +13,48 @@ ruby ./hw_13/lib/script_13_01.rb apple banana
 
 # Commands to execute Homeworks # 14
 
-# ruby ./lib/script_14_01.rb mac_address
-# ruby ./lib/script_14_02.rb ipv4_address
-# ruby ./lib/script_14_03.rb ipv6_address
+# ruby ./hw_14/lib/script_14_01.rb mac_address
+# ruby ./hw_14/lib/script_14_02.rb ipv4_address
+# ruby ./hw_14/lib/script_14_03.rb ipv6_address
 
 # Commands to execute Homeworks # 15
 
-# ruby ./lib/script_15_01_PC.rb subnet_mask
-# ruby ./lib/script_15_01_Mac.rb subnet_mask
+# ruby ./hw_15/lib/script_15_01_PC.rb subnet_mask
+# ruby ./hw_15/lib/script_15_01_Mac.rb subnet_mask
 
 # Commands to execute Homeworks # 16
 
-# ruby ./lib/script_16_01.rb
-# ruby ./lib/script_16_02.rb
+# ruby ./hw_16/lib/script_16_01.rb
+# ruby ./hw_16/lib/script_16_02.rb
 
 # Commands to execute Homeworks # 17
 
-# ruby ./lib/script_17_01.rb -f apple -s banana
-# ruby ./lib/script_17_02.rb -f apples -s bananas
-# ruby ./lib/script_17_03.rb  --first_number 100 --second_number 10
-# ruby ./lib/script_17_04.rb  --first_octet 66 --second_octet 166 --third_octet 202 --fourth_octet 14
-# ruby ./lib/script_17_05.rb  --first_number 35 --second_number 45 --third_number 61 --fourth_number 59 --fifth_number 73
-# ruby ./lib/script_17_06.rb   -a 23 -b 15 -c 97 -d 45 -e 365 -f 1234 -g 523665 -h 45 -i 2971 -j 22145
-# ruby ./lib/script_17_07.rb   --sentence Wednesday morning, John Smith is walking on the street
-# ruby ./lib/script_17_08.rb   --first_season Spring --second_season Summer --third_season Fall --fourth_season Winter
+# ruby ./hw_17/lib/script_17_01.rb -f apple -s banana
+# ruby ./hw_17/lib/script_17_02.rb -f apples -s bananas
+# ruby ./hw_17/lib/script_17_03.rb  --first_number 100 --second_number 10
+# ruby ./hw_17/lib/script_17_04.rb  --first_octet 66 --second_octet 166 --third_octet 202 --fourth_octet 14
+# ruby ./hw_17/lib/script_17_05.rb  --first_number 35 --second_number 45 --third_number 61 --fourth_number 59 --fifth_number 73
+# ruby ./hw_17/lib/script_17_06.rb   -a 23 -b 15 -c 97 -d 45 -e 365 -f 1234 -g 523665 -h 45 -i 2971 -j 22145
+# ruby ./hw_17/lib/script_17_07.rb   --sentence Wednesday morning, John Smith is walking on the street
+# ruby ./hw_17/lib/script_17_08.rb   --first_season Spring --second_season Summer --third_season Fall --fourth_season Winter
 
 # Commands to execute Homeworks # 18
 
-# ruby ./lib/script_18_01.rb -f apple -s banana
-# ruby ./lib/script_18_02.rb -f apples -s bananas
-# ruby ./lib/script_18_03.rb  --first_number 100 --second_number 10
-# ruby ./lib/script_18_04.rb  --first_octet 66 --second_octet 166 --third_octet 202 --fourth_octet 14
-# ruby ./lib/script_18_05.rb  --first_number 35 --second_number 45 --third_number 61 --fourth_number 59 --fifth_number 73
-# ruby ./lib/script_18_06.rb   -a 23 -b 15 -c 97 -d 45 -e 365 -f 1234 -g 523665 -h 45 -i 2971 -j 22145
-# ruby ./lib/script_18_07.rb   --sentence Wednesday morning, John Smith is walking on the street
-# ruby ./lib/script_18_08.rb   --first_season Spring --second_season Summer --third_season Fall --fourth_season Winter
+# ruby ./hw_18/lib/script_18_01.rb -f apple -s banana
+# ruby ./hw_18/lib/script_18_02.rb -f apples -s bananas
+# ruby ./hw_18/lib/script_18_03.rb  --first_number 100 --second_number 10
+# ruby ./hw_18/lib/script_18_04.rb  --first_octet 66 --second_octet 166 --third_octet 202 --fourth_octet 14
+# ruby ./hw_18/lib/script_18_05.rb  --first_number 35 --second_number 45 --third_number 61 --fourth_number 59 --fifth_number 73
+# ruby ./hw_18/lib/script_18_06.rb   -a 23 -b 15 -c 97 -d 45 -e 365 -f 1234 -g 523665 -h 45 -i 2971 -j 22145
+# ruby ./hw_18/lib/script_18_07.rb   --sentence Wednesday morning, John Smith is walking on the street
+# ruby ./hw_18/lib/script_18_08.rb   --first_season Spring --second_season Summer --third_season Fall --fourth_season Winter
 
+# Commands to execute Homeworks # 19
+
+# ruby ./hw_19/lib/script_19_01.rb --domain www.shopping.com > ./hw_19/report/shopping.txt
+# ruby ./hw_19/lib/script_19_01.rb --domain www.alegro.com > ./hw_19/report/alegro.txt
+# ruby ./hw_19/lib/script_19_01.rb --domain www.epinions.com > ./hw_19/report/epinions.txt
+
+# Commands to execute Homeworks # 20
+
+# ruby ./hw_20/lib/epinions_post_qa.rb -f html -o ./reports/epinions_post_qa_test.html
